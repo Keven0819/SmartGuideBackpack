cf7e0ddb7b1eec2c740387e2706db2a299b9f887
imac-3570 <imac-3570@imac-3570s-MacBook-Pro.local>
2025-11-26 17:07:35 +0800

    把WebSocket的Domain改成正確的

--- VisuallyImpairedApp/ViewModels/MainViewModel.swift
+++ VisuallyImpairedApp/ViewModels/MainViewModel.swift
@@ -19,7 +19,7 @@ class MainViewModel: ObservableObject {
     
     // MARK: - WebSocket
     private var webSocketTask: URLSessionWebSocketTask?
-    private let wsURL = URL(string: "ws://192.168.1.11:3001/ws/ios")!
+    private let wsURL = URL(string: "ws://192.168.2.7:3001/ws/ios")!
     
     // MARK: - Location
     private var locationCancellable: AnyCancellable?
